cc032c24116ef85247e3241dd53efe738cc8744b
Daniel Shumway <shumway.danny@gmail.com>
2014-11-07 17:36:21 -0500

    Added some framerate controls, tried to play around with a background that really doesn't work.

--- site/src/assets/scripts/canvas_manager.js
+++ site/src/assets/scripts/canvas_manager.js
@@ -11,9 +11,48 @@
 		if(e.use_dimension === "x") {
 			return number*(e.display_resolution.x/e.internal_resolution.x);
 		} else if (e.use_dimension === "y") {
-			return number*(e.display_resolution.x/e.internal_resolution.x);
+			return number*(e.display_resolution.y/e.internal_resolution.y);
 		} else {
 			throw "Improper dimension specified for conversion: canvas_manager: _g";
 		}
+	}
+
+	window._l = function(number){
+		var e = canvas_environment;
+		if(e.use_dimension === "x") {
+			return number*(e.internal_resolution.x/e.display_resolution.x);
+		} else if (e.use_dimension === "y") {
+			return number*(e.internal_resolution.y/e.display_resolution.y);
+		} else {
+			throw "Improper dimension specified for conversion: canvas_manager: _l";
+		}
+	}
+
+	window._animate = function(framerate, callback) {
+		var handler = {
+			"running":true,
+			"currentFrame":0,
+			"framerate":framerate
+		};
+
+		var lastTime = undefined;
+		var elapsed = 0;
+		(function checkFrame(){
+			var time = new Date().getTime();
+			if(lastTime) { elapsed += time - lastTime; }
+			lastTime = time;
+
+			var target = 1000/handler.framerate;
+			if(elapsed > target){
+				elapsed = 0;
+				callback();
+			}
+
+			if(handler.running) {
+				requestAnimationFrame(checkFrame);
+			}
+		})();
+
+		return handler;
 	}
 })();
