24c07e2d3902a43743c8788324aac076fbea2e4c
Daniel Shumway <shumway.danny@gmail.com>
2014-11-08 12:22:54 -0500

    Large speed improvements on homepage

--- site/src/assets/scripts/backgrounds/home.js
+++ site/src/assets/scripts/backgrounds/home.js
@@ -1,6 +1,7 @@
 window.canvas_environment.internal_resolution = {"x":500, "y":100};
 var canvas = document.getElementById("background-canvas");
 var ctx = canvas.getContext('2d');
+var _1 = _g(1);
 
 
 var Cloud = function(x, y, speed, radius) {
@@ -22,22 +23,15 @@ var Cloud = function(x, y, speed, radius) {
 			ctx.fillStyle = 'rgb(255, 255, 255)';
 			for(var y = this.y - piece.radius; y < this.y + piece.radius; y++){
 				var level = Math.min(Math.abs(y - (this.y - piece.radius)), Math.abs(y - (this.y + piece.radius)));
-				for(var x = this.x - level; x < this.x + level; x++){
-					ctx.fillRect(_g(x), _g(y), _g(1), _g(1) );
-				}
+				ctx.fillRect(_g(this.x-level), _g(y), _g(level*2), _1);
 			}
-
-			/*ctx.beginPath();
-			ctx.arc(_g(this.x + piece.x), _g(this.y + piece.y), _g(piece.radius), 0, 2 * Math.PI);
-			ctx.fill();*/
 		}
 	};
 
 	this.update = function() {
 		this.x -= this.speed;
-		console.log()
 		if(this.x < 0) {
-			this.x = 510;
+			this.x = 510+Math.random()*20;
 			this.y = Math.floor(Math.random()*100);
 		}
 	};
@@ -50,26 +44,14 @@ var Cloud = function(x, y, speed, radius) {
 				var level = Math.min(Math.abs(y - (this.y - piece.radius*1.2)), Math.abs(y - (this.y + piece.radius*1.2)));
 				for(var x = this.x - level; x < this.x + level; x++){
 					x = Math.floor(x);
-					//ctx.fillRect(_g(x-distance), _g(y-distance), _g(1), _g(1) );
 					if(y-distance >= ground.area[x-distance]) {
-						ctx.fillRect(_g(x-distance), _g(y-distance), _g(1), _g(1) );
+						ctx.fillRect(_g(x-distance), _g(y-distance), _1, _1 );
 					}
 				}
 			}
 		}
 	};
 };
-
-var clouds = [];
-for(var i = 0; i < 30; i++){ 
-	clouds.push(new Cloud(Math.floor(Math.random()*500), Math.floor(Math.random()*100), Math.random()*.3, 20));
-}
-
-var sprites = [];
-
-for(var i = 0; i < 10; i++){ 
-	sprites.push(new Cloud(Math.floor(Math.random()*500), Math.floor(Math.random()*100), Math.random()*.3+.2, 5));
-}
 
 var Ground = function(color, max_elevation){
 
@@ -99,7 +81,7 @@ var Ground = function(color, max_elevation){
 	this.draw = function(){
 		ctx.fillStyle=color;
 		for(var i = 0; i < this.area.length; i++){
-			ctx.fillRect(_g(i), _g(this.area[i]), _g(1), _g(200));
+			ctx.fillRect(_g(i), _g(this.area[i]), _1, _1*150);
 		}
 	};
 
@@ -107,8 +89,6 @@ var Ground = function(color, max_elevation){
 
 	}
 };
-
-var ground = new Ground('rgb(100, 200, 150', 100);
 
 var Sky = function(r, g, b) {
 
@@ -129,13 +109,32 @@ var Sky = function(r, g, b) {
 	this.draw = function(){
 		for(var i = 0; i<10; i++){
 			ctx.fillStyle='rgb('+ this.breaks[i].r +', '+ this.breaks[i].g+', '+ this.breaks[i].b+')';
-			ctx.fillRect(0, _g(this.breaks[i].y), _g(500), _g(100));
+			ctx.fillRect(0, _g(this.breaks[i].y), 500*_1, 100*_1);
 		}
 	}
 };
 
-var sky = new Sky(100, 100, 255);
+//-----------------------------------------------------------------------------------------------
 
+var sky, ground, sprites, clouds;
+var init = function() {
+	sky = new Sky(100, 100, 255);
+	ground = new Ground('rgb(100, 200, 150', 100);
+
+	var i;
+	clouds = []; for(i = 0; i < 30; i++){ 
+		clouds.push(new Cloud(Math.floor(Math.random()*500), Math.floor(Math.random()*100), Math.random()*.3, 20));
+	}
+	sprites = []; for(i = 0; i < 10; i++){ 
+		sprites.push(new Cloud(Math.floor(Math.random()*500), Math.floor(Math.random()*100), Math.random()*.3+.2, 5));
+	}
+
+
+	//Start
+	window._animate(draw, 25);
+}
+
+//-------------------------------------------------------------------------------------------------
 
 var draw = function() {
 	ctx.fillStyle = 'rgb(200, 200, 255)';
@@ -150,15 +149,14 @@ var draw = function() {
 
 	ground.draw();
 
-
 	for(sprite in sprites){
 		sprites[sprite].castShadow(ground, -8);
 	}
-	
+
 	for(sprite in sprites){
 		sprites[sprite].draw();
 		sprites[sprite].update();
 	}
-}
+};
 
-window._animate(draw, 30);
+init();
